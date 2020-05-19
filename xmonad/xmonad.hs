7e636624b2d7daedbda0742dfaf7ffc482257fc3
aidangreenlee <aidangreenlee@gmail.com>
2020-05-19 15:32:18 -0400

    commit

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -491,7 +491,7 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 --
 -- By default, do nothing.
 myStartupHook = do
-  setWMName "AidanReksScrubsWM"
+  setWMName "LG3D"
   spawn		"bash ~/.xmonad/startup.sh"
   setDefaultCursor xC_left_ptr
 
